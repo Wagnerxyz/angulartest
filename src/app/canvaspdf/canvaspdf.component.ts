148ff9cb0ae6e523177b8bcbd0126d6f25557363
Benming Wang <BWang31@slb.com>
2019-05-03 21:30:03 +0800

    run summary字段 布局设计

--- src/app/canvaspdf/canvaspdf.component.ts
+++ src/app/canvaspdf/canvaspdf.component.ts
@@ -6,13 +6,19 @@ import html2canvas from 'html2canvas'
 @Component({
   selector: 'app-canvaspdf',
   templateUrl: './canvaspdf.component.html',
-  styleUrls: ['./canvaspdf.component.css']
+  styleUrls: ['./canvaspdf.component.scss']
 })
 export class CanvaspdfComponent {
 
   //https://rawgit.com/MrRio/jsPDF/master/docs/jsPDF.html configuration compress,precision
   private pdf: jspdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
-  get now(): string { return Date(); }
+  get now(): string {
+    return new Date().toLocaleTimeString("en-GB", {
+      weekday: "long", year: "numeric", month: "long",
+      day: "numeric"
+    });
+  }
+
   constructor() {
     (<any>window).html2canvas = html2canvas;
   }
@@ -28,7 +34,7 @@ export class CanvaspdfComponent {
 
     //渲染效果好
     html2canvas(data, {
-      scale: 2
+      // scale: 3
     }).then(canvas => {
       // Few necessary setting options  
       var imgWidth = 208;
@@ -39,10 +45,25 @@ export class CanvaspdfComponent {
       // const imgProps= this.pdf.getImageProperties(imgData);
       // const pdfWidth = this.pdf.internal.pageSize.getWidth();
       // const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      // var width = this.pdf.internal.pageSize.getWidth();
-      // var height = this.pdf.internal.pageSize.getHeight();
-      this.pdf.addImage(canvas, 'PNG', 0, 0, imgWidth, imgHeight)
-      this.pdf.save('DNI.pdf'); 
+      var width = this.pdf.internal.pageSize.getWidth();
+      var height = this.pdf.internal.pageSize.getHeight();
+      // const contentDataURL = canvas.toDataURL('image/png');
+      this.pdf.addImage(canvas, 'PNG', 0, 0, width, height)
+
+      // var blobPDF = new Blob([this.pdf.output('blob')], { type: 'application/pdf' });
+      // var blobUrl = URL.createObjectURL(blobPDF);  //<--- THE ERROR APPEARS HERE
+      // window.open(blobUrl, "DescriptiveWindowName");  // will open a new tab
+
+      // URL.revokeObjectURL(blobUrl);
+      this.pdf.save(canvas.width + 'DNI.pdf');
+
+      // for (let i = 0; i < 3; i++) {
+      //   let pdf = new jspdf('p', 'px', 'a4');
+      //   canvas.width = canvas.width - 200;
+      //   pdf.addImage(canvas, 'PNG', 0, 0, canvas.width, canvas.height)
+      //   pdf.save(canvas.width + 'DNI.pdf');
+      // }
+
     });
   }
 
