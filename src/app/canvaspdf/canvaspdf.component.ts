077d8eb75786b2a2f62b5fe2175a5c0b869a1c3c
Benming Wang <BWang31@slb.com>
2019-05-03 21:30:03 +0800

    run summary字段 布局设计 css grid

--- src/app/canvaspdf/canvaspdf.component.ts
+++ src/app/canvaspdf/canvaspdf.component.ts
@@ -12,29 +12,37 @@ export class CanvaspdfComponent {
 
   //https://rawgit.com/MrRio/jsPDF/master/docs/jsPDF.html configuration compress,precision
   private pdf: jspdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+  get now(): string { return Date(); }
   constructor() {
     (<any>window).html2canvas = html2canvas;
   }
   public captureScreen() {
 
-    var data = document.getElementById('contentToConvert');
-    this.pdf.html(data, {
-      callback: function (doc) {
-        doc.save();
-      }
-    });
+    //渲染效果差
+    var data = document.getElementById('container');
+    // this.pdf.html(data, {
+    //   callback: function (doc) {
+    //     doc.save();
+    //   }
+    // });
 
-
-    html2canvas(data).then(canvas => {
+    //渲染效果好
+    html2canvas(data, {
+      scale: 2
+    }).then(canvas => {
       // Few necessary setting options  
       var imgWidth = 208;
       var pageHeight = 295;
       var imgHeight = canvas.height * imgWidth / canvas.width;
       var heightLeft = imgHeight;
 
-      const contentDataURL = canvas.toDataURL('image/png')
-      this.pdf.addImage(contentDataURL, 'PNG', 0, 0, imgWidth, imgHeight)
-      this.pdf.save('DNI.pdf'); // Generated PDF   
+      // const imgProps= this.pdf.getImageProperties(imgData);
+      // const pdfWidth = this.pdf.internal.pageSize.getWidth();
+      // const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      // var width = this.pdf.internal.pageSize.getWidth();
+      // var height = this.pdf.internal.pageSize.getHeight();
+      this.pdf.addImage(canvas, 'PNG', 0, 0, imgWidth, imgHeight)
+      this.pdf.save('DNI.pdf'); 
     });
   }
 
